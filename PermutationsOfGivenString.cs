ca9c3e7566ed1f1862c4e1249eca2a1f6e369344
Ambili das v s <ambili.kan@gmail.com>
2022-06-01 13:13:05 +0530

    Program for returning all permutations of a string is done

---
+++ PermutationsOfGivenString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class PermutationsOfGivenString
+    {
+        public static void Permute(string str,int startIndex,int endIndex)
+        {
+            if (startIndex == endIndex)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    str = Swap(str, startIndex, i);
+                    Permute(str, startIndex + 1, endIndex);
+                    str = Swap(str, startIndex, i);
+                }
+            }
+        }
+        public static string Swap(string a,int i,int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,23 @@ namespace AlgorithmPrograms
         {
             Console.WriteLine("\t\tWELCOME TO ALGORITHM PROGRAMS");
             Console.WriteLine("*******************************************************");
+            Console.WriteLine("Enter the question number for executing the program");
+            
 
+            do
+            {
+                Console.WriteLine("1: Program for returning all the permutations of a string");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        string str = "ABC";
+                        int strlength = str.Length;
+                        PermutationsOfGivenString.Permute(str, 0, strlength - 1);
+                        break;
+                }
+            }
+            while (true);
         }
     }
 }
