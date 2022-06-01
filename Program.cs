8e18bae649bda2ec4fb0e8fe39d53ae0e2672057
Ambili das v s <ambili.kan@gmail.com>
2022-06-01 12:45:39 +0530

    Project for Algorithm Programs is created

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWELCOME TO ALGORITHM PROGRAMS");
+            Console.WriteLine("*******************************************************");
+
+        }
+    }
+}
