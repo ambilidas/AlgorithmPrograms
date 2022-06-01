ae4972169e78352df026fb96f6dbdfcb6ae050f6
Ambili das v s <ambili.kan@gmail.com>
2022-06-01 14:16:19 +0530

    Program for checking two words are anagram or not is added

---
+++ Anagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class Anagram
+    {
+        public static bool CheckAnagram(string firststring, string secondstring)
+        {
+            if (firststring.Length != secondstring.Length)
+            {
+                return false;
+            }
+            //Convert string into character array
+            char[] FirstCharArray = firststring.ToLower().ToCharArray();
+            char[] SecondCharArray = secondstring.ToLower().ToCharArray();
+            //Sort array
+            Array.Sort(FirstCharArray);
+            Array.Sort(SecondCharArray);
+            //check each chara and position
+            for (int i = 0; i < FirstCharArray.Length; i++)
+            {
+                if (FirstCharArray[i].ToString() != SecondCharArray[i].ToString())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }      
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,13 +14,30 @@ namespace AlgorithmPrograms
             do
             {
                 Console.WriteLine("1: Program for returning all the permutations of a string");
+                Console.WriteLine("6: Program for checking whether 2 strings are Anagram or not");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        string str = "ABC";
+                        string str = "ABCD";
                         int strlength = str.Length;
                         PermutationsOfGivenString.Permute(str, 0, strlength - 1);
+                        break;
+                     case 6:
+                        string firststring,secondstring;
+                        Console.WriteLine("Enter first string");
+                        firststring = Console.ReadLine();
+                        Console.WriteLine("Enter second string");
+                        secondstring = Console.ReadLine();
+                        //check words are anagram or not
+                        if(Anagram.CheckAnagram(firststring,secondstring) == true)
+                        {
+                            Console.WriteLine("The words are Anagram!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Words are not Anagram!!");
+                        }
                         break;
                 }
             }
